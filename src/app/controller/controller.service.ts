d185cb57c641dd08e57e80a916d6ee123f4e1d72
shaul.almog@sapiens.com <shaul.almog@sapiens.com>
2017-02-19 10:51:28 +0200

    controller internal execute

--- src/app/controller/controller.service.ts
+++ src/app/controller/controller.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@angular/core';
 
+import { Observable } from 'rxjs/Observable';
+import { Subscriber } from 'rxjs/Subscriber';
+
 import { ConfigService, ProcessConfig } from '../config';
 import { ControllerState } from './controller-state';
 import { StepStrategiesService } from './step-strategies';
@@ -23,23 +26,36 @@ export class ControllerService {
     }
 
     execute(input?) {
+        return Observable.create(subscriber => {
+            this.internalExecute(input, subscriber);
+        });
+    }
+
+    private selectStrategy() {
+        let strategy = this.findStrategy();
+        if (!strategy.enter()) {
+            this.internalExecute();
+        }
+    }
+
+    private internalExecute(input?, subscriber?: Subscriber) {
         // Merge in the data from the UI
         this.controllerState.update(input);
 
         let strategy = this.findStrategy();
         strategy.execute(this.controllerState)
             .subscribe(result => {
+                if (subscriber) {
+                    subscriber.complete();
+                }
                 // Merge in the data from the execution
                 this.controllerState.update(result);
                 this.selectStrategy();
+            }, error => {
+                if (subscriber) {
+                    subscriber.error(error);
+                }
             });
-    }
-
-    private selectStrategy() {
-        let strategy = this.findStrategy();
-        if (!strategy.enter()) {
-            this.execute();
-        }
     }
 
     private findStrategy() {
