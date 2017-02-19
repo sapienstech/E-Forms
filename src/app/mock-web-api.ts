55816024eff94a5caddf817f5449207b36b0f35d
shaul.almog@sapiens.com <shaul.almog@sapiens.com>
2017-02-19 17:17:26 +0200

    Added more mocks and moved shim file form the index.html to the cli config

--- src/app/mock-web-api.ts
+++ src/app/mock-web-api.ts
@@ -10,26 +10,36 @@ import {
 import { ExecutionInput } from './model';
 
 const FLOW_MANIFESTS = [
-    { id: '1000', data: require('../data/manifest/form1.json') },
-    { id: '2000', data: require('../data/manifest/form2.json') },
-    { id: '3000', data: require('../data/manifest/form3.json') },
+    {id: '1000', data: require('../data/manifest/form1.json')},
+    {id: '2000', data: require('../data/manifest/form2.json')},
+    {id: '3000', data: require('../data/manifest/form3.json')},
 ];
 
+const LAYOUTS = [
+    {id: '1000', data: require('../data/layout/layout1.json')},
+    {id: '2000', data: require('../data/layout/layout2.json')},
+    {id: '3000', data: require('../data/layout/layout3.json')},
+];
+
+
 const PROCESSES = [
-    { title: 'Process 1', steps: [] },
+    {title: 'Process 1', steps: []},
     {
         title: 'Process 2',
         description: 'Process 2 Description',
         steps: [
             {
+                type: 'ui',
                 flow: '1000',
                 title: 'Flow 1 - Step 1'
             },
             {
+                type: 'ui',
                 flow: '2000',
                 title: 'Flow 2 - Step 2'
             },
             {
+                type: 'execute',
                 flow: '3000',
                 title: 'Flow 3 - Step 3'
             }
@@ -38,9 +48,9 @@ const PROCESSES = [
 ];
 
 const EXECUTION_RESULTS = [
-    {flow:'1000',result: require('../data/execution-result/result1.json')},
-    {flow:'2000',result: require('../data/execution-result/result2.json')},
-    {flow:'3000',result: require('../data/execution-result/result3.json')}
+    {flow: '1000', result: require('../data/execution-result/result1.json')},
+    {flow: '2000', result: require('../data/execution-result/result2.json')},
+    {flow: '3000', result: require('../data/execution-result/result3.json')}
 ];
 
 export class MockWebApi extends InMemoryDbService {
@@ -51,14 +61,15 @@ export class MockWebApi extends InMemoryDbService {
 
         this.responseInterceptor = (res) => {
             let body = (res.body as any).data;
-            return res.merge({ body });
+            return res.merge({body});
         };
     }
 
     createDb() {
         return {
             flowManifests: FLOW_MANIFESTS,
-            process: PROCESSES
+            process: PROCESSES,
+            layouts:LAYOUTS
         };
     }
 
