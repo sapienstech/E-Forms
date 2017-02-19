d884c19839950e7029b6fbeae47cbb58cf3f2fdc
Schmulik Raskin <schmuli@gmail.com>
2017-02-19 16:13:41 +0200

    (cont) Use empty subscriber pattern

--- src/app/controller/controller.service.ts
+++ src/app/controller/controller.service.ts
@@ -1,11 +1,20 @@
 import { Injectable } from '@angular/core';
 
 import { Observable } from 'rxjs/Observable';
-import { Subscriber } from 'rxjs/Subscriber';
 
 import { /* ConfigService, */ ProcessConfig } from '../config';
 import { ControllerState } from './controller-state';
 import { StepStrategiesService } from './step-strategies';
+
+interface ExecuteSubscriber {
+    complete(): void;
+    error(error: any): void;
+}
+
+const EmptyExecuteSubscriber: ExecuteSubscriber = {
+    complete() { },
+    error(_) { }
+};
 
 @Injectable()
 export class ControllerService {
@@ -30,28 +39,23 @@ export class ControllerService {
     private selectStrategy() {
         let strategy = this.findStrategy();
         if (!strategy.enter()) {
-            this.internalExecute();
+            this.internalExecute(EmptyExecuteSubscriber);
         }
     }
 
-    private internalExecute(input?, subscriber?: Subscriber<void>) {
+    private internalExecute(subscriber: ExecuteSubscriber, data?) {
         // Merge in the data from the UI
-        this.controllerState.update(input);
+        this.controllerState.update(data);
 
         let strategy = this.findStrategy();
         strategy.execute(this.controllerState)
             .subscribe(result => {
-                if (subscriber) {
-                    subscriber.complete();
-                }
+                subscriber.complete();
+
                 // Merge in the data from the execution
                 this.controllerState.update(result);
                 this.selectStrategy();
-            }, error => {
-                if (subscriber) {
-                    subscriber.error(error);
-                }
-            });
+            }, error => subscriber.error(error));
     }
 
     private findStrategy() {
