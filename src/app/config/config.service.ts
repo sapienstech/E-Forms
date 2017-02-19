93dea9ab2b683923eedc72c6eaff115f6abb7472
shaul almog <shaul.almog@sapiens.com>
2017-02-19 17:54:55 +0200

    mock server api  (#28)
    * mock server
    
    * (cont) Small fixes for compiler/angular errors
    
    * mock server
    
    * Added more mocks and moved shim file form the index.html to the cli config

--- src/app/config/config.service.ts
+++ src/app/config/config.service.ts
@@ -5,10 +5,19 @@ import { ProcessConfig } from './config.model';
 
 @Injectable()
 export class ConfigService {
-    constructor(private http: Http) { }
+    constructor(private http: Http) {
+    }
 
     getProcessConfig() {
         return this.http.get('/api/process')
             .map(response => response.json() as ProcessConfig[]);
+    }
+
+    getManifest(flowId) {
+        return this.http.get('api/flowManifests/' + flowId).map(response => response.json());
+    }
+
+    getLayout(flowId){
+        return this.http.get('api/layouts/' + flowId).map(response => response.json());
     }
 }
--- src/app/mock-web-api.ts
+++ src/app/mock-web-api.ts
@@ -9,26 +9,49 @@ import {
 
 import { ExecutionInput } from './model';
 
-const FLOWS = [
-    { id: '123', data: {} }
+const FLOW_MANIFESTS = [
+    {id: '1000', data: require('../data/manifest/form1.json')},
+    {id: '2000', data: require('../data/manifest/form2.json')},
+    {id: '3000', data: require('../data/manifest/form3.json')},
 ];
 
+const LAYOUTS = [
+    {id: '1000', data: require('../data/layout/layout1.json')},
+    {id: '2000', data: require('../data/layout/layout2.json')},
+    {id: '3000', data: require('../data/layout/layout3.json')},
+];
+
+
 const PROCESSES = [
-    { title: 'Process 1', steps: [] },
+    {title: 'Process 1', steps: []},
     {
         title: 'Process 2',
         description: 'Process 2 Description',
         steps: [
             {
-                flow: 'flow1',
+                type: 'ui',
+                flow: '1000',
                 title: 'Flow 1 - Step 1'
+            },
+            {
+                type: 'ui',
+                flow: '2000',
+                title: 'Flow 2 - Step 2'
+            },
+            {
+                type: 'execute',
+                flow: '3000',
+                title: 'Flow 3 - Step 3'
             }
         ]
     }
 ];
 
-const EXECUTION_RESULTS = {
-};
+const EXECUTION_RESULTS = [
+    {flow: '1000', result: require('../data/execution-result/result1.json')},
+    {flow: '2000', result: require('../data/execution-result/result2.json')},
+    {flow: '3000', result: require('../data/execution-result/result3.json')}
+];
 
 export class MockWebApi extends InMemoryDbService {
     private responseInterceptor: ResponseInterceptor;
@@ -38,14 +61,15 @@ export class MockWebApi extends InMemoryDbService {
 
         this.responseInterceptor = (res) => {
             let body = (res.body as any).data;
-            return res.merge({ body });
+            return res.merge({body});
         };
     }
 
     createDb() {
         return {
-            flows: FLOWS,
-            process: PROCESSES
+            flowManifests: FLOW_MANIFESTS,
+            process: PROCESSES,
+            layouts:LAYOUTS
         };
     }
 
