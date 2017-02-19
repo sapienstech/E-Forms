b08f0ba591598835167d65a13f415bf8a3a826f1
Schmulik Raskin <schmuli@gmail.com>
2017-02-19 15:32:20 +0200

    (cont) Small fixes for compiler/angular errors

--- src/app/controller/flow-executor.service.ts
+++ src/app/controller/flow-executor.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
-import { ExecutionInput, IoFactType } from '../model/execution';
+import { ExecutionInput, ArtifactKey } from '../model/execution';
 
 @Injectable()
 export class FlowExecutorService {
@@ -8,30 +8,61 @@ export class FlowExecutorService {
     }
 
     execute(flowId: string, input) {
-
         // TODO: Prepare JSON input
-        let executionInputs = this.transformFormInputsToDEExecutionInputs(input);
-        return this.http.post(`/flow/${ flowId }`, input);
+        let executionInputs = this.transformFormInputsToDEExecutionInputs(flowId, input);
+        return this.http.post(`/flow/${ flowId }`, executionInputs);
         // TODO: Parse JSON output
     }
 
-    public transformFormInputsToDEExecutionInputs(input) {
-        let inputFields = Object.keys(input.value);
-        let ioWrapper = [];
-        let executionInput = {rootGroup: {IoGroupInstances: ioWrapper}};
-        for (let field of inputFields) {
-            let ft = <IoFactType>{};
-            ft.factTypeName = field;
-            ft.values = [input.value[field]];
-            ft.isConclusionValues = false;
-            ioWrapper.push({IoFactTypes:[ft]});
-        }
-        return executionInput;
+    transformFormInputsToDEExecutionInputs(flowId: string, input) {
+        // let inputFields = Object.keys(input.value);
+        // let ioWrapper = [];
+        // let executionInput = { rootGroup: { IoGroupInstances: ioWrapper } };
+        // for (let field of inputFields) {
+        //     let ft = <IoFactType>{};
+        //     ft.factTypeName = field;
+        //     ft.values = [input.value[field]];
+        //     ft.isConclusionValues = false;
+        //     ioWrapper.push({ IoFactTypes: [ft] });
+        // }
+        // return executionInput;
+
+        let artifactKey: ArtifactKey = {
+            name: flowId,
+            releaseName: undefined,
+            tagName: undefined,
+            artifactType: 'FLOW'
+        };
+
+        let result: ExecutionInput = {
+            executableKey: {
+                artifactKey
+            },
+            executionInput: {
+                rootGroup: {
+                    IoGroupInstances: Object.keys(input)
+                        .map(key => {
+                            return {
+                                IoFactTypes: [
+                                    {
+                                        factTypeName: key,
+                                        values: [input[key]],
+                                        isConclusionValues: false
+                                    }
+                                ]
+                            };
+                        })
+                }
+            },
+            executionConfiguration: null
+        };
+
+        return result;
     }
 
-    public transformDEExecutionResultsToFormInputs(executionResults){
-        for(let result of executionResults){
+    // transformDEExecutionResultsToFormInputs(executionResults) {
+    //     for (let result of executionResults) {
 
-        }
-    }
+    //     }
+    // }
 }
