75c6e3f75e6bfcdd31936244a24143ac2d00cde5
Schmulik Raskin <schmuli@gmail.com>
2017-02-22 16:15:11 +0200

    Add missing RxJS imports for operators (#40)

--- src/app/config/config.service.ts
+++ src/app/config/config.service.ts
@@ -3,6 +3,8 @@ import { Http } from '@angular/http';
 
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/observable/of';
+import 'rxjs/add/observable/combineLatest';
+import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
 
 import { Manifest, FormLayout } from '../model';
--- src/app/controller/step-strategies/async-step-strategy.ts
+++ src/app/controller/step-strategies/async-step-strategy.ts
@@ -1,8 +1,5 @@
 import { Injectable } from '@angular/core';
 
-import { Observable } from 'rxjs/Observable';
-import 'rxjs/add/observable/empty';
-
 import { ControllerState } from '../controller-state';
 import { FlowExecutorService } from '../flow-executor.service';
 
@@ -15,7 +12,6 @@ export class AsyncStepStrategy extends StepStrategy {
     }
 
     execute(state: ControllerState) {
-        this.executeFlowAsync(state);
-        return Observable.empty<any>();
+        return this.executeFlowAsync(state);
     }
 }
