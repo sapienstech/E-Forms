33fa068a6a2e3277e8324570a37ab777721dde1c
shaul almog <shaul.almog@sapiens.com>
2017-02-26 11:00:00 +0200

    added passThruUnknownUrl to bypass the mock api on other rest calls (#45)
    add config component to show process config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,12 @@
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
-
 import { DashboardModule } from './dashboard';
 import { ConfigModule } from './config';
 import { ProcessSelectionModule } from './process-selection';
 import { ControllerModule } from './controller';
 import { EformModule } from './eform';
-
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { MockWebApi } from './mock-web-api';
 
 import { AppRouterModule } from './app-router.module';
@@ -22,9 +19,7 @@ import { AppComponent } from './app.component';
     imports: [
         BrowserModule,
         HttpModule,
-
-        InMemoryWebApiModule.forRoot(MockWebApi),
-
+        InMemoryWebApiModule.forRoot(MockWebApi,{passThruUnknownUrl : true}),
         AppRouterModule,
         DashboardModule,
         ConfigModule,
