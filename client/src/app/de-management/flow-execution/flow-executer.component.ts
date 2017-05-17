2115bdb110d0dd33b28e1cb473f41f5a20df8e30
shaul.almog@sapiens.com <shaul.almog@sapiens.com>
2017-05-17 14:59:09 +0300

    DECISION-12697  - Installation and Bug fixes

--- client/src/app/de-management/flow-execution/flow-executer.component.ts
+++ client/src/app/de-management/flow-execution/flow-executer.component.ts
@@ -27,7 +27,7 @@ export class FlowExecuterComponent implements OnInit {
     executionResult: any = {};
     @ViewChild('form') form: FormComponent;
     private errorMessage: string;
-    // private executing: boolean = false;
+    private executing: boolean = false;
 
     constructor(private route: ActivatedRoute, private service: ManagementServiceFacade) {
 
@@ -66,13 +66,17 @@ export class FlowExecuterComponent implements OnInit {
 
     clear(){
         this.form.reset();
+        this.errorMessage = null;
+        this.executionResult = null;
     }
 
 
     execute(){
-        // this.executing = true;
+        this.executing = true;
         this.errorMessage = null;
         this.service.execute(this.output,this.de).subscribe(result=>{
+            this.executing = false;
+
             if(result.error){
                this.errorMessage = result.error;
             }
